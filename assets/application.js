69403f0693ac0172c48db1f7f57d2c52434103e9
hasankarim18 <hallysust@gmail.com>
2025-07-22 06:53:35 +0600

    first

---
+++ assets/application.js
@@ -0,0 +1,7 @@
+// Put your application javascript here
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Hello from assets/application.js");
+  /// other code goes here
+});
+//console
